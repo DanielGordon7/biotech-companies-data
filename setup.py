0b52561f5015570209486a34a6809353555b8052
DanielGordon7 <62928645+DanielGordon7@users.noreply.github.com>
2023-11-15 03:57:16 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='biotech-companies-data',
+    version='0.1',
+    description='A dataset of biotech companies.',
+    author='Daniel Gordon',
+    url='https://github.com/DanielGordon7/biotech-companies-data',
+)
